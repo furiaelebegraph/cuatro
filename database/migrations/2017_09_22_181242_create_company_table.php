1850c24c53858497f51897e6998c2ee2602d3ee3
furiaelebegraph <webmaster@elebegraph.com>
2017-10-26 18:07:48 -0500

    Agregue carpetas que faltaban

--- database/migrations/2017_09_22_181242_create_company_table.php
+++ database/migrations/2017_09_22_181242_create_company_table.php
@@ -14,6 +14,8 @@ class CreateCompanyTable extends Migration
     public function up()
     {
         Schema::create('company', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
             $table->string('nombre');
             $table->text('direccion');
             $table->string('imagen');
